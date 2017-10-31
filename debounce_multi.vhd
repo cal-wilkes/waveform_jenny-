a3c78e73e5b5419d81ca2575d440965704e0b8ca
lawrencechiu12 <31356619+lawrencechiu12@users.noreply.github.com>
2017-10-31 16:01:38 -0600

    Debouncer

--- debounce_multi.vhd
+++ debounce_multi.vhd
@@ -8,10 +8,10 @@ USE ieee.std_numeric_std.all;
 ENTITY multi_debounce IS
   PORT(
     clk     : IN  STD_LOGIC;  --input clock
-    buttons  : IN  STD_LOGIC_VECTOR (2 downto 0);  --input signal to be debounced
-	swtiches : in std_logic_vector (14 downto 0);
-	d_buttons : in std_logic_vector (2 downto 0);
-	d_swtiches : in std_logic_vector (14 downto 0)
+    buttons  : IN  STD_LOGIC_VECTOR (3 downto 0);  --input signal to be debounced
+	swtiches : in std_logic_vector (1 downto 0);
+	d_buttons : in std_logic_vector (3 downto 0);
+	d_swtiches : in std_logic_vector (1 downto 0)
 	);
 END multi_debounce;
 
@@ -42,6 +42,12 @@ ARCHITECTURE Behavioral OF multi_debounce IS
 					result => d_buttons(2)
 					);
 					
+	button_3: debouncer
+		port map (clk => clk,
+					button => buttons(3),
+					result => d_buttons(3)
+					);
+					
 	swtich_0: debouncer
 		port map (clk => clk,
 					button => switches(0),
@@ -54,89 +60,6 @@ ARCHITECTURE Behavioral OF multi_debounce IS
 					result => d_switches(1)
 					);		
     
-	swtich_2: debouncer
-		port map (clk => clk,
-					button => switches(2),
-					result => d_switches(2)
-					);
-					
-	swtich_3: debouncer
-		port map (clk => clk,
-					button => switches(3),
-					result => d_switches(3)
-					);				
-
-    swtich_4: debouncer
-		port map (clk => clk,
-					button => switches(4),
-					result => d_switches(4)
-					);
-					
-	swtich_5: debouncer
-		port map (clk => clk,
-					button => switches(5),
-					result => d_switches(5)
-					);
-
-	swtich_6: debouncer
-		port map (clk => clk,
-					button => switches(6),
-					result => d_switches(6)
-					);
-
-	swtich_7: debouncer
-		port map (clk => clk,
-					button => switches(7),
-					result => d_switches(7)
-					);				
-
-	swtich_8: debouncer
-		port map (clk => clk,
-					button => switches(8),
-					result => d_switches(8)
-					);
-					
-	swtich_9: debouncer
-		port map (clk => clk,
-					button => switches(9),
-					result => d_switches(9)
-					);
-
-	swtich_10: debouncer
-		port map (clk => clk,
-					button => switches(10),
-					result => d_switches(10)
-					);
-					
-	swtich_11: debouncer
-		port map (clk => clk,
-					button => switches(11),
-					result => d_switches(11)
-					);
-
-	swtich_12: debouncer
-		port map (clk => clk,
-					button => switches(12),
-					result => d_switches(12)
-					);
-					
-	swtich_13: debouncer
-		port map (clk => clk,
-					button => switches(13),
-					result => d_switches(13)
-					);
-					
-	swtich_14: debouncer
-		port map (clk => clk,
-					button => switches(14),
-					result => d_switches(14)
-					);
-					
-	swtich_15: debouncer
-		port map (clk => clk,
-					button => switches(15),
-					result => d_switches(15)
-					);
 
 					
 END Behavioral;
